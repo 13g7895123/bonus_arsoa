174a0db5cee3fc9f08dd0d8fe85b1e3c77b187e1
Jarvis <13g7895123@gmail.com>
2025-03-16 12:27:45 +0800

    移除資料庫

--- controllers/eform/Api.php
+++ controllers/eform/Api.php
@@ -3,6 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Api extends MY_Controller
 {
+    // private $db;
+
     public function __construct()
     {
         parent::__construct();
@@ -17,6 +19,8 @@ class Api extends MY_Controller
         $this->load->model( 'eform/Form7Model' );
         
         $this->load->library( 'user_agent' );
+
+        // $this->db = $this->load->database('default', true);
     }
 
     public function form1()
@@ -88,10 +92,11 @@ class Api extends MY_Controller
             redirect( 'member/login' );
         }
 
-        $msconn = $this->front_mssql_model->ms_connect(); 
-
-        $postData = $this->input->post();
-        print_r($postData); die();
+        print_r(123); die();
+        // $this->fileUpload();
+        // print_r($_FILES); die();
+        // $commonModel = new CommonModel();
+        // $commonModel->uploadFile($_FILES);
     }
 
     public function form6()
@@ -148,5 +153,59 @@ class Api extends MY_Controller
             ->set_output(json_encode($result));
     }
     
-    
+    // public function fileUpload()
+    // {        
+    //     if (!isset($_FILES['signature'])) {
+    //         throw new Exception("沒有上傳的檔案");
+    //     }
+
+    //     // 設定上傳資料夾 - IIS路徑
+    //     // $uploadPath = $_SERVER['DOCUMENT_ROOT'] . "/public/uploads/images/";
+    //     // $uploadPath = $_SERVER['DOCUMENT_ROOT'] . "/public/uploads/";
+    //     $filePath = "public/func/eform/";
+    //     $uploadPath = $_SERVER['DOCUMENT_ROOT'] . "/" . $filePath;
+        
+    //     // 取得上傳檔案資訊
+    //     $file = $_FILES['signature'];
+    //     print_r($file); die();
+    //     $fileTmpPath = $file['tmp_name'];
+    //     $fileName = "sign_" . time() . ".png";
+    //     $destination = $uploadPath . $fileName;
+    //     $originalFileName = $file['name'];      // 取得原始檔名
+    //     $fileType = $file['type'];             // 取得檔案類型 MIME type
+    //     $fileSize = $file['size'];             // 取得檔案大小(bytes)
+
+    //     // 取得原始檔名
+    //     $originalFileName = $file['name'];
+
+    //     // 檢查是否為有效圖片
+    //     $imageInfo = @getimagesize($fileTmpPath);
+    //     if ($imageInfo === false) {
+    //         throw new Exception("無效的圖片格式");
+    //     }
+
+    //     // 檢查圖片類型
+    //     $allowedTypes = [IMAGETYPE_PNG, IMAGETYPE_JPEG];
+    //     if (!in_array($imageInfo[2], $allowedTypes)) {
+    //         throw new Exception("只允許PNG或JPEG格式");
+    //     }
+
+    //     // 移動檔案到上傳目錄
+    //     // print_r($fileTmpPath); die();
+    //     if (!move_uploaded_file($fileTmpPath, $destination)) {
+    //         throw new Exception("檔案儲存失敗: " . error_get_last()['message']);
+    //     }
+
+    //     // 寫入資料表
+    //     $insertData = array(
+    //         'name' => $originalFileName,
+    //         'path' => $filePath . $fileName,
+    //         'type' => $fileType,
+    //         'size' => $fileSize,
+    //         'upload_at' => date('Y-m-d H:i:s')
+    //     );
+    //     $this->db->insert('eform_file', $insertData);
+
+    //     return True;
+    // }
 }
