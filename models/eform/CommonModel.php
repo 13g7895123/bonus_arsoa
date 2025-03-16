49a097f430c2bce03a403e27d1477b2a79f01b56
Jarvis <13g7895123@gmail.com>
2025-03-16 14:06:07 +0800

    共用檔案上傳

---
+++ models/eform/CommonModel.php
@@ -0,0 +1,72 @@
+<?php
+class CommonModel extends CI_Model
+{
+    private $db;
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->db = $this->load->database('default', true);
+    }
+
+    /**
+     * 上傳檔案
+     * @param array $fileData 上傳的檔案資料
+     * @param string $name 檔案名稱
+     * @return array 上傳結果，包含是否成功和插入的ID
+     */
+    public function uploadFile($fileData, $name)
+    {
+        if (!isset($fileData[$name])) {
+            throw new Exception("沒有上傳的檔案");
+        }
+
+        // 設定上傳資料夾 - IIS路徑
+        $filePath = "public/func/eform/";
+        $uploadPath = $_SERVER['DOCUMENT_ROOT'] . "/" . $filePath;
+        
+        // 取得上傳檔案資訊
+        $file = $fileData[$name];
+        $fileTmpPath = $file['tmp_name'];
+        $fileName = str_replace('.', '', uniqid(rand(), true)) . ".png";
+        $destination = $uploadPath . $fileName;
+        $originalFileName = $file['name'];      // 取得原始檔名
+        $fileType = $file['type'];             // 取得檔案類型 MIME type
+        $fileSize = $file['size'];             // 取得檔案大小(bytes)
+
+        // 檢查是否為有效圖片
+        $imageInfo = @getimagesize($fileTmpPath);
+        if ($imageInfo === false) {
+            throw new Exception("無效的圖片格式");
+        }
+
+        // 檢查圖片類型
+        $allowedTypes = [IMAGETYPE_PNG, IMAGETYPE_JPEG];
+        if (!in_array($imageInfo[2], $allowedTypes)) {
+            throw new Exception("只允許PNG或JPEG格式");
+        }
+
+        // 移動檔案到上傳目錄
+        if (!move_uploaded_file($fileTmpPath, $destination)) {
+            throw new Exception("檔案儲存失敗: " . error_get_last()['message']);
+        }
+
+        // 寫入資料表
+        $insertData = array(
+            'name' => $originalFileName,
+            'path' => $filePath . $fileName,
+            'type' => $fileType,
+            'size' => $fileSize,
+            'uploaded_at' => date('Y-m-d H:i:s')
+        );
+        $insertId = $this->insertData($insertData);
+
+        return [True, $insertId];
+    }
+
+    public function insertData($data)
+    {
+        $this->db->insert('eform_file', $data);
+        return $this->db->insert_id();
+    }
+}
